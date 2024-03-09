bcf06d44a7df8adb88789e81dd6180140b289695
Lemon_Snatcher <sierrafer04@gmail>
2024-03-08 20:52:18 -0800

    Added wager function/Spruced up some of the lines

--- SlotMachine.py
+++ SlotMachine.py
@@ -1,5 +1,7 @@
 
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
 
 #Continually asks user to give us a valid amount until we get a proper amount
@@ -26,15 +28,33 @@ def get_number_of_lines():
             if 1 <= lines <= MAX_LINES:
                 break
             else:
-                print("Must be a valid number of lines.")
+                print("Only 1 to 3 lines a wager.")
         else:
-            print("Please enter a valid whole number.")
+            print("Please enter a valid number of lines.")
     return lines
 
+#Same as the previous ask for proper wager and wont break until condition is met
+def get_bet():
+    while True:
+        wager = input("What's your wager per line? $")
+        if wager.isdigit():
+            wager = int(wager)
+            if MIN_BET <= wager <= MAX_BET:
+                break
+            else:
+                print(f"Wagers run from ${MIN_BET} to ${MAX_BET}.")
+        else:
+            print("Please enter a proper wager.")
+
+    return wager
 
 def main():
     balance = deposit()
     lines = get_number_of_lines()
+    wager = get_bet()
+    total_wager = wager * lines
+    print(f"Current Wager is ${wager} on {lines} lines. Total wager is: ${total_wager}")
+
     print(balance, lines)
 
 main()
